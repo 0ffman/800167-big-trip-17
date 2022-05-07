d087c7952f4d1ca28c75a0c7c1d809629c5f4ce4
Ruslan <Snow88ilike@gmail.com>
2022-05-07 16:45:02 +0300

    удалил point-new-edit, уменьшил количество разделений на компоненты

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -1,14 +1,12 @@
 import SortView from '../view/sort-view.js';
 import TripListView from '../view/trip-list-view.js';
-import TripListItemView from '../view/trip-list-item-view.js';
 import PointView from '../view/point-view.js';
 import PointEditView from '../view/point-edit-view.js';
-import { render, RenderPosition } from '../render.js';
+import { render } from '../render.js';
 
 export default class TripPresenter {
 
   tripListComponent = new TripListView();
-  tripListItemComponent = new TripListItemView();
   pointEditComponent = new PointEditView();
 
   init = (eventContainer, pointsModel) => {
@@ -20,18 +18,9 @@ export default class TripPresenter {
     render(new SortView(), this.eventContainer);
     render(this.tripListComponent, this.eventContainer);
 
+    render(new PointEditView(editPoint), this.tripListComponent.getElement());
     for (let i = 0; i < this.eventPoints.length; i++) {
-      render(new TripListItemView(), this.tripListComponent.getElement());
+      render(new PointView(this.eventPoints[i]), this.tripListComponent.getElement());
     }
-
-    const tripEventElements = Array.from(document.querySelector('.trip-events__list').children);
-
-    tripEventElements.forEach((item, index) => {
-      //console.log(item, index);
-      render(new PointView(this.eventPoints[index]), item);
-    });
-
-    render(this.tripListItemComponent, this.tripListComponent.getElement(), RenderPosition.AFTERBEGIN);
-    render(new PointEditView(editPoint), this.tripListItemComponent.getElement());
   };
 }
